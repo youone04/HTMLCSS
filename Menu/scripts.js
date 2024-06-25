2eda3fdb8c09df530c429a0b2e9b467740a57d9e
youone04 <yudi.gunaone87@gmail.com>
2024-06-25 07:14:05 +0700

    first commit

---
+++ Menu/scripts.js
@@ -0,0 +1,50 @@
+let lastScrollTop = 0;
+const menu = document.getElementById('menu');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    console.log(scrollTop)
+
+    if (scrollTop <= 0) {
+        // User scrolls up
+        menu.style.display = 'none';
+    } else {
+        // User scrolls down
+        menu.style.display = 'block';
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+}, false);
+
+
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://youtu.be/9sNQFJAb3Ss?si=i6TRY6tqlEunv_vd";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Create an <iframe> (and YouTube player) after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: 'YOUR_VIDEO_ID', // Ganti dengan ID video YouTube Anda
+        playerVars: {
+            autoplay: 1,
+            loop: 1,
+            playlist: 'YOUR_VIDEO_ID', // Ganti dengan ID video YouTube Anda untuk loop
+            controls: 0,
+            showinfo: 0,
+            modestbranding: 1,
+            mute: 1,
+            rel: 0
+        },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.mute();
+    event.target.playVideo();
+}
